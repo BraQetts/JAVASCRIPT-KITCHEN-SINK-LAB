9b104068e8c8ddbd3f867861f602ec94a463088a
Ben Nduwuba <braqetts@gmail.com>
2018-08-17 10:15:27 +0100

    JS KITCHEN SINK DRILL PART SOLUTIONS

--- app.js
+++ app.js
@@ -1,1 +1,141 @@
-console.log("hello World !")
+var MYNAME='ben';
+
+const USSTATES= 52;
+
+
+console.log(USSTATES);
+
+
+var result= 5+4 ;
+
+console.log(result);
+
+
+
+
+
+//Alerts when a number starts with a letter before or fater L
+
+/*
+var name="Ben" ;
+
+
+var x= name.charCodeAt(1);
+
+
+if(x>76){
+
+alert('Next !');
+
+
+}else{
+
+alert('Back of the Line !!!');
+
+}
+
+*/
+
+// this function alerts Hello World !
+/*
+function sayHello(){
+
+alert('Hello World!');
+
+}
+
+sayHello();
+
+*/
+
+
+
+/*
+
+
+function checkAge(name,age){
+
+    var name="Chris";
+    var age=12;
+
+if (age<21){
+
+    alert("Sorry You aren't Old Enough to view this page");
+
+}
+
+
+}
+
+checkAge();
+
+*/
+
+
+/*
+var i;
+var vegetables=['carrots','cucumber','okro', 'water melon'];
+
+for(var i=0; i<vegetables.length; i++ ){
+
+console.log(vegetables[i]);
+
+}
+
+*/
+
+
+var bio=[  
+
+    {name:'Ben',
+     Age: 25
+    },
+
+    {name:'Ben',
+    Age: 25
+    },
+   {name:'Ben',
+    Age: 25
+    },
+    {name:'Ben',
+     Age: 25
+   },
+    {name:'Ben',
+     Age: 25
+    }
+
+
+]
+
+bio.forEach()
+
+
+
+
+
+
+
+
+
+/*
+
+function getLength(word){
+    var long = word.length;
+    return long;
+   }
+   
+   var howFar = getLength('Benedic');
+
+   console.log(howFar);
+
+   if (howFar % 2===0){
+
+alert('The World is nice and even !')
+
+   }else{
+
+alert('The World is an Odd place !')
+
+   }
+
+   */
